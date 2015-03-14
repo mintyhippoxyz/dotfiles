86bf325d31b31dd651a4448d433b91be73e84949
Matt Adams <matta@rtvision.com>
2015-03-13 23:37:25 -0500

    new bash prompt thanks to https://github.com/Wilto/dotfiles

--- bash.d/30_prompt.sh
+++ bash.d/30_prompt.sh
@@ -1,40 +1,118 @@
 #!/bin/bash
 
-# Highlight the user name when logged in as root.
-if [[ "${USER}" == "root" ]]; then
-	userStyle="${red}";
-else
-	userStyle="${orange}";
-fi;
+tput sgr 0 0
 
-# Highlight the hostname when connected via SSH.
-if [[ "${SSH_TTY}" ]]; then
-	hostStyle="${bold}${red}";
-else
-	hostStyle="${yellow}";
-fi;
+# Base styles and color palette
+# Solarized colors
+# https://github.com/altercation/solarized/tree/master/iterm2-colors-solarized
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+SOLAR_YELLOW=$(tput setaf 136)
+SOLAR_ORANGE=$(tput setaf 166)
+SOLAR_RED=$(tput setaf 124)
+SOLAR_MAGENTA=$(tput setaf 125)
+SOLAR_VIOLET=$(tput setaf 61)
+SOLAR_BLUE=$(tput setaf 33)
+SOLAR_CYAN=$(tput setaf 37)
+SOLAR_GREEN=$(tput setaf 64)
+SOLAR_WHITE=$(tput setaf 254)
+style_user="\[${RESET}${SOLAR_ORANGE}\]"
+style_host="\[${RESET}${SOLAR_YELLOW}\]"
+style_path="\[${RESET}${SOLAR_GREEN}\]"
+style_chars="\[${RESET}${SOLAR_WHITE}\]"
+style_branch="${SOLAR_CYAN}"
 
-# Set the terminal title to the current working directory.
-PS1="\$(eyeballs)" # eyeballs
-PS1+="\[\033]0;\w\007\]";
-PS1+="\[${bold}\]";
-PS1+="\[${userStyle}\]\u"; # username
-PS1+="\[${white}\] at ";
-# if ldap common name is set show in place of actual hostname
-test -s /etc/ldapcn && export LDAPCN=`cat /etc/ldapcn`
-if [ -z "$LDAPCN" ]; then
-	PS1+="\[${hostStyle}\]\h"; # hostname
-else
-	PS1+="\[${hostStyle}\]${LDAPCN}"; # ldap common name
+if [[ "$SSH_TTY" ]]; then
+	# connected via ssh
+	style_host="\[${BOLD}${SOLAR_RED}\]"
+elif [[ "$USER" == "root" ]]; then
+	# logged in as root
+	style_user="\[${BOLD}${SOLAR_RED}\]"
 fi
-PS1+="\[${white}\] in ";
-PS1+="\[${green}\]\w"; # working directory
-PS1+="\$(git_prompt \"${white} on ${violet}\")"; # Git repository details
-PS1+="\n";
-PS1+="\[${white}\]\$ \[${reset}\]"; # `$` (and reset color)
-PS2="\[${yellow}\] \[${reset}\]";
 
-export PS1;
-export PS2;
+eyeballs()
+{
+	if [ $? != 0 ]; then
+		printf "${SOLAR_RED}( O_o) ${RESET}"
+	fi
+}
+
+is_git_repo()
+{
+	$(git rev-parse --is-inside-work-tree &> /dev/null)
+}
+
+is_git_dir()
+{
+	$(git rev-parse --is-inside-git-dir 2> /dev/null)
+}
+
+get_git_branch()
+{
+	local branch_name
+	# Get the short symbolic ref
+	branch_name=$(git symbolic-ref --quiet --short HEAD 2> /dev/null) ||
+		# If HEAD isn't a symbolic ref, get the short SHA
+	branch_name=$(git rev-parse --short HEAD 2> /dev/null) ||
+		# Otherwise, just give up
+	branch_name="(unknown)"
+	printf $branch_name
+}
+
+# Git status information
+prompt_git()
+{
+	local git_info git_state uc us ut st
+	if ! is_git_repo || is_git_dir; then
+		return 1
+	fi
+	git_info=$(get_git_branch)
+	# Check for uncommitted changes in the index
+	if ! $(git diff --quiet --ignore-submodules --cached); then
+		uc="${SOLAR_GREEN}+"
+	fi
+	# Check for unstaged changes
+	if ! $(git diff-files --quiet --ignore-submodules --); then
+		us="${SOLAR_RED}!"
+	fi
+	# Check for untracked files
+	if [ -n "$(git ls-files --others --exclude-standard)" ]; then
+		ut="${SOLAR_ORANGE}?"
+	fi
+	# Check for stashed files
+	if $(git rev-parse --verify refs/stash &>/dev/null); then
+		st="${SOLAR_YELLOW}$"
+	fi
+	git_state=$uc$us$ut$st
+	# Combine the branch name and state information
+	if [[ $git_state ]]; then
+		git_info="$git_info${SOLAR_CYAN}[$git_state${SOLAR_CYAN}]"
+	fi
+	# You know you're on `gh-pages`, right? *Right*?
+	if [[ $git_info == "gh-pages" ]]; then
+		style_branch="${SOLAR_BLUE}"
+	fi
+	# Don't screw up `stable`.
+	if [[ $git_info == *stable* ]]; then
+		style_branch="${SOLAR_RED}"
+	fi
+	printf "${SOLAR_WHITE} on ${style_branch}${git_info}"
+}
+
+# Set the terminal title to the current working directory
+PS1="\[\033]0;\w\007\]"
+
+# Build the prompt
+PS1+="\$(eyeballs)" # Eyeballs
+PS1+="${style_user}\u" # Username
+PS1+="${style_chars}@" # @
+PS1+="${style_host}\h" # Host
+PS1+="${style_chars}: " # :
+PS1+="${style_path}\w" # Working directory
+PS1+="\$(prompt_git)" # Git details
+PS1+="\n" # Newline
+PS1+="${style_chars}\$ \[${RESET}\]" # $ (and reset color)
+
+PS2="\[${SOLAR_YELLOW}] \[${RESET}]"
 
 # vim: ft=sh
