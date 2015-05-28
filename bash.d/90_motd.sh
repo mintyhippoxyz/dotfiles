c99f7aa4360a3bf8d842b993452c3e6a64d429b5
Matt Adams <matta@rtvision.com>
2015-05-28 17:49:48 -0500

    pipe fortune to cowsay where available

--- bash.d/90_motd.sh
+++ bash.d/90_motd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-test -e /usr/bin/fortune && fortune && echo "";
+test -e /usr/bin/fortune && test -e /usr/bin/cowsay && fortune | cowsay
 echo "Welcome to $(hostname)";
 uptime | xargs;
 uname -rmp;
