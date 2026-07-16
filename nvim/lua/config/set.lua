a0844ca4efe9b07846b9816cead02d423645df23
Matt Adams <matta@rtvision.com>
2026-07-16 11:14:19 -0500

    chore: update tmux and nvim settings

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -14,7 +14,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
-vim.opt.wrap = false
+vim.opt.wrap = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
@@ -54,8 +54,8 @@ vim.opt.splitbelow = true
 -- horizontal split to the right
 vim.opt.splitright = true
 
--- disable mouse
-vim.opt.mouse = ''
+-- enable mouse in all modes
+vim.opt.mouse = 'a'
 
 vim.api.nvim_create_autocmd("BufReadPost", {
 	pattern = { "*" },
